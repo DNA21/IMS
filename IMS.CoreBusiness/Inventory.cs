f79f50078bba2b458dad398216f53c10965c8e76
Daniel Galuardi <dan.galuardi@gmail.com>
2023-03-09 16:13:37 -0500

    Add Inventory Page

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,10 +1,18 @@
-﻿namespace IMS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required]
         public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0")]
         public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "The price must be greater or equal to 0")]
         public double Price { get; set; }
     }
 }
