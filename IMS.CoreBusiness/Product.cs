0040491cf0f7c540fc39370515b1b3429ec4484a
Daniel Galuardi <dan.galuardi@gmail.com>
2023-03-10 13:38:48 -0500

    Ensure Product Price is Greater Than Inventory Cost Validation

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IMS.CoreBusiness.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -20,6 +21,8 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
         public double Price { get; set; }
+
+        [Product_EnsurePriceIsGreaterThanInventoriesCost]
 
         public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
 
